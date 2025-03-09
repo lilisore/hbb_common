babf106e92c8152ebbef822ed548a00ba9b316d1
lilisore <121156073+lilisore@users.noreply.github.com>
2025-03-10 01:36:28 +0800

    Modify the relay server domain name and key

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rust.882883.xyz"];
+pub const RS_PUB_KEY: &str = "hGJv6bDxaE+pAqGTgp9KiSG3YX7dnhg9uG6xZQfGpcU=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
